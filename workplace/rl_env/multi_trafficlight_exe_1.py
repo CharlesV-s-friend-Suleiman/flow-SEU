213676422bac90a92fdccb74257ffb1e1c476325
W-G-O <156505075+W-G-O@users.noreply.github.com>
2024-03-12 17:12:08 +0800

    Add files via upload

---
+++ workplace/rl_env/multi_trafficlight_exe_1.py
@@ -0,0 +1,200 @@
+"""Multi-agent traffic light example (single shared policy)."""
+
+from ray.rllib.agents.ppo.ppo_policy import PPOTFPolicy
+from flow.envs.multiagent.multi_trafficlight_e1 import MultiTrafficLightGrid_a_Env
+from flow.networks.traffic_light_grid import SingleIntersectionNet
+from flow.core.params import SumoParams, EnvParams, InitialConfig, NetParams
+from flow.core.params import InFlows, SumoCarFollowingParams, VehicleParams
+from flow.controllers import SimCarFollowingController, GridRecycleRouter,GridRouter
+from ray.tune.registry import register_env
+from flow.utils.registry import make_create_env
+import json
+import ray
+from ray.rllib.agents.registry import get_agent_class
+from ray.tune import run_experiments
+from flow.utils.rllib import FlowParamsEncoder
+
+
+# Experiment parameters
+N_ROLLOUTS = 5  # number of rollouts per training iteration
+N_CPUS = 10  # number of parallel workers
+
+# Environment parameters
+HORIZON = 500  # time horizon of a single rollout
+V_ENTER = 10  # enter speed for departing vehicles
+INNER_LENGTH = 300  # length of inner edges in the traffic light grid network
+LONG_LENGTH = 700  # length of final edge in route
+SHORT_LENGTH = 500  # length of edges that vehicles start on
+# number of vehicles originating in the left, right, top, and bottom edges
+N_LEFT, N_RIGHT, N_TOP, N_BOTTOM = 1, 1, 1, 1
+
+EDGE_INFLOW = 1800  # inflow rate of vehicles at every edge
+N_ROWS = 3  # number of row of bidirectional lanes
+N_COLUMNS = 4  # number of columns of bidirectional lanes
+
+
+# we place a sufficient number of vehicles to ensure they confirm with the
+# total number specified above. We also use a "right_of_way" speed mode to
+# support traffic light compliance
+vehicles = VehicleParams()
+num_vehicles = (N_LEFT + N_RIGHT) * N_COLUMNS + (N_BOTTOM + N_TOP) * N_ROWS
+vehicles.add(
+    veh_id="human",
+    acceleration_controller=(SimCarFollowingController, {}),
+    car_following_params=SumoCarFollowingParams(
+        min_gap=2.5,
+        max_speed=V_ENTER,
+        decel=7.5,  # avoid collisions at emergency stops
+        speed_mode="right_of_way",
+    ),
+    routing_controller=(GridRecycleRouter, {}),
+    num_vehicles=num_vehicles)
+
+# inflows of vehicles are place on all outer edges (listed here)
+outer_edges = []
+outer_edges += ["left{}_{}".format(N_ROWS, i) for i in range(N_COLUMNS)]
+outer_edges += ["right0_{}".format(i) for i in range(N_ROWS)]
+outer_edges += ["bot{}_0".format(i) for i in range(N_ROWS)]
+outer_edges += ["top{}_{}".format(i, N_COLUMNS) for i in range(N_ROWS)]
+
+# equal inflows for each edge (as dictate by the EDGE_INFLOW constant)
+inflow = InFlows()
+for edge in outer_edges:
+    inflow.add(
+        veh_type="human",
+        edge=edge,
+        vehs_per_hour=EDGE_INFLOW,
+        depart_lane="free",
+        depart_speed=V_ENTER)
+
+flow_params = dict(
+    # name of the experiment
+    exp_tag="grid_0_{}x{}_i{}_multiagent".format(N_ROWS, N_COLUMNS, EDGE_INFLOW),
+
+    # name of the flow environment the experiment is running on
+    env_name=MultiTrafficLightGrid_a_Env,
+
+    # name of the network class the experiment is running on
+    network=SingleIntersectionNet,
+
+    # simulator that is used by the experiment
+    simulator='traci',
+
+    # sumo-related parameters (see flow.core.params.SumoParams)
+    sim=SumoParams(
+        restart_instance=True,
+        sim_step=1,
+        render=False,
+        print_warnings=False,
+    ),
+
+    # environment related parameters (see flow.core.params.EnvParams)
+    env=EnvParams(
+        horizon=HORIZON,
+        warmup_steps=0,
+        evaluate=False,
+        additional_params={
+            "target_velocity": 30,
+            "switch_time": 3.0,
+            "num_observed": 4,
+            "discrete": True,
+            "tl_type": "controlled",
+            "num_local_edges": 4,
+            "num_local_lights": 4,
+        },
+    ),
+
+    # network-related parameters (see flow.core.params.NetParams and the
+    # network's documentation or ADDITIONAL_NET_PARAMS component)
+    net=NetParams(
+        inflows=inflow,
+        additional_params={
+            "speed_limit": 30,  # inherited from grid0 benchmark
+            "grid_array": {
+                "short_length": SHORT_LENGTH,
+                "inner_length": INNER_LENGTH,
+                "long_length": LONG_LENGTH,
+                "row_num": N_ROWS,
+                "col_num": N_COLUMNS,
+                "cars_left": N_LEFT,
+                "cars_right": N_RIGHT,
+                "cars_top": N_TOP,
+                "cars_bot": N_BOTTOM,
+            },
+            "horizontal_lanes": 3,
+            "vertical_lanes": 3,
+        },
+    ),
+
+    # vehicles to be placed in the network at the start of a rollout (see
+    # flow.core.params.VehicleParams)
+    veh=vehicles,
+
+    # parameters specifying the positioning of vehicles upon initialization
+    # or reset (see flow.core.params.InitialConfig)
+    initial=InitialConfig(
+        spacing='custom',
+        shuffle=True,
+    ),
+)
+
+
+def setup_exps():
+    """Return the relevant components of an RLlib experiment.
+
+    Returns
+    -------
+    str
+        name of the training algorithm
+    str
+        name of the gym environment to be trained
+    dict
+        training configuration parameters
+    """
+    alg_run = "PPO"
+
+    agent_cls = get_agent_class(alg_run)
+    config = agent_cls._default_config.copy()
+    config["num_gpus"] = 1
+    config["num_workers"] = 10
+    config["train_batch_size"] = HORIZON * N_ROLLOUTS
+    config["gamma"] = 0.999  # discount rate
+    config["model"].update({"fcnet_hiddens": [3, 3]})
+    config["use_gae"] = True
+    config["lambda"] = 0.97
+    config["vf_clip_param"] = 1000
+    config["kl_target"] = 0.02
+    config["num_sgd_iter"] = 10
+    config['clip_actions'] = False  # FIXME(ev) temporary ray bug
+    config["horizon"] = HORIZON
+
+    # save the flow params for replay
+    flow_json = json.dumps(
+        flow_params, cls=FlowParamsEncoder, sort_keys=True, indent=4)
+    config['env_config']['flow_params'] = flow_json
+    config['env_config']['run'] = alg_run
+
+    create_env, gym_name = make_create_env(params=flow_params, version=0)
+
+    # Register as rllib env
+    register_env(gym_name, create_env)
+    return alg_run, gym_name, config
+
+
+alg_run, gym_name, config = setup_exps()
+ray.init(num_cpus=N_CPUS + 1,num_gpus=1)
+trials = run_experiments({
+    flow_params["exp_tag"]: {
+        "run": alg_run,
+        "env": gym_name,
+        "config": {
+            **config
+        },
+        "checkpoint_freq": 20,
+        "checkpoint_at_end": True,
+        "max_failures": 999,
+        "stop": {
+            "training_iteration": 30,
+        },
+    }
+})
